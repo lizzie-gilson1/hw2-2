9676f20296866d980ec6f246daf4e90081dbac26
Jeffy <jeffy_jeffy@berkeley.edu>
2024-03-06 15:10:15 -0600

    serial test implementation

--- mpi.cpp
+++ mpi.cpp
@@ -1,20 +1,78 @@
 #include "common.h"
 #include <mpi.h>
+#include <cmath>
+#include <vector>
 
-// Put any static global variables here that you will use throughout the simulation.
+// Apply the force from neighbor to particle
+void apply_force(particle_t& particle, particle_t& neighbor) {
+    // Calculate Distance
+    double dx = neighbor.x - particle.x;
+    double dy = neighbor.y - particle.y;
+    double r2 = dx * dx + dy * dy;
+
+    // Check if the two particles should interact
+    if (r2 > cutoff * cutoff)
+        return;
+
+    r2 = fmax(r2, min_r * min_r);
+    double r = sqrt(r2);
+
+    // Very simple short-range repulsive force
+    double coef = (1 - cutoff / r) / r2 / mass;
+    particle.ax += coef * dx;
+    particle.ay += coef * dy;
+}
+
+//Integrate the ODE
+void move(particle_t& p, double size) {
+    // Slightly simplified Velocity Verlet integration
+    // Conserves energy better than explicit Euler method
+    p.vx += p.ax * dt;
+    p.vy += p.ay * dt;
+    p.x += p.vx * dt;
+    p.y += p.vy * dt;
+
+    // Bounce from walls
+    while (p.x < 0 || p.x > size) {
+        p.x = p.x < 0 ? -p.x : 2 * size - p.x;
+        p.vx = -p.vx;
+    }
+
+    while (p.y < 0 || p.y > size) {
+        p.y = p.y < 0 ? -p.y : 2 * size - p.y;
+        p.vy = -p.vy;
+    }
+}
 
 void init_simulation(particle_t* parts, int num_parts, double size, int rank, int num_procs) {
-	// You can use this space to initialize data objects that you may need
-	// This function will be called once before the algorithm begins
-	// Do not do any particle simulation here
+    // You can use this space to initialize data objects that you may need
+    // This function will be called once before the algorithm begins
+    // Do not do any particle simulation here
 }
 
 void simulate_one_step(particle_t* parts, int num_parts, double size, int rank, int num_procs) {
-    // Write this function
+    // Reset forces to zero for all particles
+    for (int i = 0; i < num_parts; i++) {
+        parts[i].ax = 0;
+        parts[i].ay = 0;
+    }
+
+    // Apply forces between all pairs of particles
+    for (int i = 0; i < num_parts; i++) {
+        for (int j = 0; j < num_parts; j++) {
+            if (i != j) { // Avoid self-interaction
+                apply_force(parts[i], parts[j]);
+            }
+        }
+    }
+
+    // Move particles based on the calculated forces
+    for (int i = 0; i < num_parts; i++) {
+        move(parts[i], size);
+    }
+    
 }
 
 void gather_for_save(particle_t* parts, int num_parts, double size, int rank, int num_procs) {
-    // Write this function such that at the end of it, the master (rank == 0)
-    // processor has an in-order view of all particles. That is, the array
-    // parts is complete and sorted by particle id.
+    // some code here 
 }
